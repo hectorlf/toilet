9d3bb4d6b7203a68382687d4f71052400683573b
hectorlf <hectorlopezfernandez@hotmail.com>
2016-12-26 01:45:22 +0100

    Fix the remaining LocaleResolverTests

--- src/test/java/com/hectorlopezfernandez/blog/metadata/LocaleResolverTests.java
+++ src/test/java/com/hectorlopezfernandez/blog/metadata/LocaleResolverTests.java
@@ -24,36 +24,29 @@ public class LocaleResolverTests extends BaseMvcTest {
 		// this should kick in as an accept-language locale
 		mockMvc.perform(get("/index.page").locale(Locale.forLanguageTag("es-ES")))
 			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("Bienvenido")));
+			.andExpect(content().string(containsString("Inicio")));
 	}
 
 	@Test
 	public void testAcceptHeaderLocale2() throws Exception {
-		mockMvc.perform(get("/index.page").locale(Locale.forLanguageTag("en-GB")))
+		// this should kick in as an accept-language locale
+		mockMvc.perform(get("/index.page").locale(Locale.forLanguageTag("en")))
 			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("Welcome")));
+			.andExpect(content().string(containsString("Home")));
 	}
 
 	@Test
 	public void testAcceptHeaderLocale3() throws Exception {
-		mockMvc.perform(get("/index.page").locale(Locale.forLanguageTag("en-US")).header("Accept-Language","en-US, en-GB, en"))
+		mockMvc.perform(get("/index.page").header("Accept-Language","es-ES, es, en"))
 			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("Welcome")));
+			.andExpect(content().string(containsString("Inicio")));
 	}
 
 	@Test
 	public void testAcceptHeaderLocale4() throws Exception {
-		mockMvc.perform(get("/index.page").locale(Locale.forLanguageTag("pt-BR")).header("Accept-Language","pt-BR, pt, en"))
+		mockMvc.perform(get("/index.page").header("Accept-Language","pt-BR, pt, es"))
 			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("Welcome")));
-	}
-
-	@Test
-	public void testAcceptHeaderLocale5() throws Exception {
-		// this should kick in as an accept-language locale
-		mockMvc.perform(get("/index.page").locale(Locale.forLanguageTag("en-GB")))
-			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("Welcome")));
+			.andExpect(content().string(containsString("Inicio")));
 	}
 
 }

224b32047f104befb977570f35374432cbbbca1b
hectorlf <hectorlopezfernandez@hotmail.com>
2020-08-17 01:40:13 +0200

    Add support for serving Page objects, e.g. /pages/about

--- src/main/java/com/hectorlopezfernandez/blog/page/PageService.java
+++ src/main/java/com/hectorlopezfernandez/blog/page/PageService.java
@@ -1,8 +1,10 @@
 package com.hectorlopezfernandez.blog.page;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,14 +13,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class PageService {
 
-	@Autowired
-	private PageRepository pageRepository;
+	private final PageRepository pageRepository;
+
+	@Inject
+	public PageService(PageRepository pageRepository) {
+		this.pageRepository = pageRepository;
+	}
 
 	/**
 	 * Returns a list of pages tailored for the sitemap
 	 */
 	public List<Page> listPagesForSitemap() {
 		return pageRepository.findAllByPublishedIsTrue();
+	}
+
+	/**
+	 * Returns the Page identified by its slug
+	 */
+	public Optional<Page> getPageBySlug(String slug) {
+		return Optional.ofNullable(pageRepository.findBySlug(slug));
 	}
 
 }

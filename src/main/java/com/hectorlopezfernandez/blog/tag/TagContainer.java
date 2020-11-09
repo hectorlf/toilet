1aea164ed6c71b4ed93b9265b96be9a70c14ca82
hectorlf <hectorlopezfernandez@hotmail.com>
2020-11-09 22:52:30 +0100

    Refactor rest element containers

--- src/main/java/com/hectorlopezfernandez/blog/tag/TagContainer.java
+++ src/main/java/com/hectorlopezfernandez/blog/tag/TagContainer.java
@@ -2,25 +2,12 @@ package com.hectorlopezfernandez.blog.tag;
 
 import java.util.List;
 
-public class TagContainer {
+import com.hectorlopezfernandez.blog.ElementContainer;
 
-	private List<Tag> tags;
-	private int total;
-	
-	public TagContainer(List<Tag> tags, int total) {
-		if (tags == null) throw new IllegalArgumentException("Parameter tags cannot be null");
-		this.tags = tags;
-		this.total = total;
-	}	
+public class TagContainer extends ElementContainer<Tag> {
 
-	// getters & setters
-
-	public List<Tag> getTags() {
-		return tags;
-	}
-
-	public int getTotal() {
-		return total;
+	public TagContainer(List<Tag> elements, int total) {
+		super(elements, total);
 	}
 
 }

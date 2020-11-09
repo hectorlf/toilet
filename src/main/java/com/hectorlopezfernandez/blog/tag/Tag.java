1893b47623fb8278f2a8ecd88c8f38c48100fa71
hectorlf <hectorlopezfernandez@hotmail.com>
2020-11-09 02:25:43 +0100

    Remove unnecessary @Indexed annotation

--- src/main/java/com/hectorlopezfernandez/blog/tag/Tag.java
+++ src/main/java/com/hectorlopezfernandez/blog/tag/Tag.java
@@ -1,7 +1,6 @@
 package com.hectorlopezfernandez.blog.tag;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -19,7 +18,6 @@ public class Tag {
 
 	@Id
 	private String id;
-	@Indexed(unique=true)
 	private String slug;
 	private String name;
 	private int count;

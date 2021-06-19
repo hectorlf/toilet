ceea2e9c98f428d861093c31ce286dbb8ee524c6
hectorlf <hectorlopezfernandez@hotmail.com>
2021-06-19 13:10:01 +0200

    Remove support for port 8080. HTTPS or GTFO.
    Also, enable http2 over tcp. Don't @ me, ask spring boot.

--- src/main/java/com/hectorlopezfernandez/toilet/Application.java
+++ src/main/java/com/hectorlopezfernandez/toilet/Application.java
@@ -1,14 +1,9 @@
 package com.hectorlopezfernandez.toilet;
 
-import org.eclipse.jetty.server.NetworkTrafficServerConnector;
-import org.eclipse.jetty.server.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.jetty.JettyServerCustomizer;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 
@@ -29,20 +24,6 @@ public class Application {
 	public LocaleResolver localeResolver(MetadataService metadataService) {
 		return new CustomLocaleResolver(metadataService);
 	}
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-    	final JettyServletWebServerFactory containerFactory = new JettyServletWebServerFactory();
-    	containerFactory.addServerCustomizers(new JettyServerCustomizer() {
-			@Override
-			public void customize(Server server) {
-				NetworkTrafficServerConnector httpConnector = new NetworkTrafficServerConnector(server);
-				httpConnector.setPort(8080);
-				server.addConnector(httpConnector);
-			}
-    	});
-    	return containerFactory;
-    }
 
 	@Bean
 	public TagModelAssembler tagModelAssembler() {

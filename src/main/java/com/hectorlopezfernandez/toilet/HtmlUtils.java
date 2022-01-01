181550c1b56f67aa6bf771016aebead8476d61b7
hectorlf <hectorlopezfernandez@hotmail.com>
2022-01-01 11:57:23 +0100

    Replace JSoup Whitelist with Safelist

--- src/main/java/com/hectorlopezfernandez/toilet/HtmlUtils.java
+++ src/main/java/com/hectorlopezfernandez/toilet/HtmlUtils.java
@@ -3,7 +3,7 @@ package com.hectorlopezfernandez.toilet;
 import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
+import org.jsoup.safety.Safelist;
 import org.unbescape.html.HtmlEscape;
 
 
@@ -15,13 +15,13 @@ public final class HtmlUtils {
 
 	private static Pattern NEW_LINES = Pattern.compile("\\R");
 	private static Pattern CONTIGUOUS_PARAGRAPHS = Pattern.compile("</p><p>");
-	private static Whitelist FEED_WHITELIST = Whitelist.basicWithImages();
-	private static Whitelist INDEX_WHITELIST = Whitelist.none();
+	private static Safelist FEED_SAFELIST = Safelist.basicWithImages();
+	private static Safelist INDEX_SAFELIST = Safelist.none();
 
 	public static String preprocessForFeeds(String html) {
 		if (html == null || html.isEmpty()) return html;
 
-		return Jsoup.clean(html, FEED_WHITELIST);
+		return Jsoup.clean(html, FEED_SAFELIST);
 	}
 
 	public static String preprocessForSearchIndex(String html) {
@@ -32,7 +32,7 @@ public final class HtmlUtils {
 		System.out.println(processedContent);
 		processedContent = CONTIGUOUS_PARAGRAPHS.matcher(processedContent).replaceAll("</p> <p>");
 		System.out.println(processedContent);
-		processedContent = Jsoup.clean(processedContent, INDEX_WHITELIST);
+		processedContent = Jsoup.clean(processedContent, INDEX_SAFELIST);
 		System.out.println(processedContent);
 		return HtmlEscape.unescapeHtml(processedContent);
 	}
